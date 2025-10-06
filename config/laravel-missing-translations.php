0df5123bbb03ee97bb009bf1585fbe12b23a4b0e
Finn <71390226+FinnPaes@users.noreply.github.com>
2025-10-06 20:40:30 +0200

    Add ability to exclude translations or locales via configuration file (#1)

---
+++ config/laravel-missing-translations.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    /**
+     * An array of missing translations that you do not want to save.
+     */
+    'exclude_translations' => [
+        // Example: 'Server Error'
+    ],
+
+    /**
+     * An array of locales you do not want to save missing translations from.
+     */
+    'exclude_locales' => [
+        // Example: 'en'
+    ],
+];
